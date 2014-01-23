6d40e4189594139cbeba67843f463139aa3ea97e
Dereck Lesko <lesko@moravian.edu>
2014-01-23 13:25:54 -0500

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Lesko, Dereck on 1/23/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
